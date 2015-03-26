4489f73356ec2fc0dbeb2983ca860466dced730a
CharDale Barnes <differentaccounts@hotmail.com>
2015-03-26 07:37:10 -0500

    Added images and very basic layout for the scene.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get "/" => 'animate#demo', :as => 'demo'
+  get "/" => 'animate#demo'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
